3588abffb56703154293ab8b3a5cb4c3fa0efc10
ilfatGub <ilfat.gubaidullin@gmail.com>
2022-11-29 21:34:48 +0500

    new

---
+++ models/Lesson.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "lesson".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property string $url
+ * @property string|null $visible
+ */
+class Lesson extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'lesson';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'description', 'url'], 'required'],
+            [['name', 'description', 'url', 'visible'], 'string', 'max' => 255],
+            [['visible'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'description' => 'Description',
+            'url' => 'Url',
+            'visible' => 'Visible',
+        ];
+    }
+}
---
+++ views/user/_form.php
@@ -0,0 +1,26 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\models\User $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="user-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+    <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
+    <?= $form->field($model, 'role')->dropDownList(['100' => 'User', '200' => 'Admin']) ?>
+    <?= $form->field($model, 'password')->passwordInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
