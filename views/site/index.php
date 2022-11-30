5465216cdbdc5ac211188f325f9fce8175d0d535
ilfatGub <ilfat.gubaidullin@gmail.com>
2022-11-30 21:58:37 +0500

    add icon

--- views/site/index.php
+++ views/site/index.php
@@ -7,14 +7,22 @@ use yii\helpers\Url;
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
-    <?php if($active):?>
+    <?php if($all):?>
         <div class="body-content mt-4 mb-4">
             Уроки 
             <hr class="bg-info">
             <div class="row">
-            <?php foreach($active as $item){ ?>
+            <?php foreach($all as $item){ ?>
                 <div class="col-lg-4">
-                    <h2><?=$item->name?></h2>
+                    <h2>
+                        <?=$item->name?>
+                        <?php if( in_array( $item->id ,$passed ) ) { ?>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check-circle text-success" viewBox="0 0 16 16">
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
+                            </svg>
+                        <?php } ?>
+                    </h2>
                     <p><?= $item->_description?></p>
                     <p><a class="btn btn-outline-secondary" href="<?= Url::toRoute(['/site/lesson', 'id' => $item->id]) ?>"> Перейти </a></p>
                 </div>
