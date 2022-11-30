02a095437ef2de6e78140038054a14f1d7a8a8dc
ilfatGub <ilfat.gubaidullin@gmail.com>
2022-11-30 21:44:06 +0500

    add data

--- migrations/m221129_161855_create_lesson_table.php
+++ migrations/m221129_161855_create_lesson_table.php
@@ -23,8 +23,19 @@ class m221129_161855_create_lesson_table extends Migration
             'name' => $this->string()->notNull(),
             'description' => $this->string(1000)->notNull(),
             'url' => $this->string()->notNull(),
-            'visible' => $this->string()->notNull(),
+            'visible' => $this->integer()->notNull(),
         ], $tableOptions);
+
+
+        for($i = 0; $i <= 10; $i++){
+            $this->insert('{{%lesson}}', 
+            [
+                'name'=>'Lesson '. $i, 
+                'description'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+                'url'=>'https://www.youtube.com/embed/SrihQzC610M',
+                'visible'=>1
+        ]);
+        }
     }
 
     /**
